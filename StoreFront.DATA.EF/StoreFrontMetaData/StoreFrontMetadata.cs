3bc67120941fae8b90d9dcd86f980aa55adb5cd1
Kayla Nguyen <kaylannguyen@outlook.com>
2022-03-16 16:44:27 -0500

    metadata and loginpartial

---
+++ StoreFront.DATA.EF/StoreFrontMetaData/StoreFrontMetadata.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreFront.DATA.EF//.StoreFrontMetaData
+{
+    class StoreFrontMetadata
+    {
+        public class ProductMetadata
+        {
+            [Required(ErrorMessage ="*Required field")]
+            [StringLength(25, ErrorMessage ="*Must be 25 characters or less")]
+            [Display(Name = "Product")]
+            public string ProductName { get; set; }
+
+            [Display(Name = "Category")]
+            public int CategoryID { get; set; }
+
+            public decimal Price { get; set; }
+
+            [Required(ErrorMessage ="*Required field")]
+            [Display(Name = "Status")]
+            public int StatusID { get; set; }
+
+            [StringLength(100, ErrorMessage = "*Must be 100 characters or less")]
+            [UIHint("MultilineText")]
+            public string Description { get; set; }
+
+            [StringLength(100, ErrorMessage = "*Must be 100 characters or less")]
+            [Display(Name = "Image")]
+            public string ProductImage { get; set; }
+        }
+
+        public class EmployeeMetadata
+        {
+            //public int EmployeeID { get; set; }
+
+            [Required(ErrorMessage ="*Required field")]
+            [StringLength(25, ErrorMessage = "*Must be 25 characters or less")]
+            public string FirstName { get; set; }
+
+            [Required(ErrorMessage ="*Required field")]
+            [StringLength(25, ErrorMessage = "*Must be 25 characters or less")]
+            public string LastName { get; set; }
+
+            public int DepartmentID { get; set; }
+
+            [StringLength(25, ErrorMessage = "*Must be 25 characters or less")]
+            public string Position { get; set; }
+
+            public int DirectReportID { get; set; }
+        }
+
+        public class CategoryMetadata
+        {
+            //public int CategoryID { get; set; }
+
+            [Required(ErrorMessage = "*Required field")]
+            [StringLength(25, ErrorMessage = "*Must be 25 characters or less")]
+            public string CategoryName { get; set; }
+        }
+
+        public class DepartmentMetadata
+        {
+            //public int DepartmentID { get; set; }
+
+            [Required(ErrorMessage = "*Required field")]
+            [StringLength(50, ErrorMessage = "*Must be 50 characters or less")]
+            public string DepartmentName { get; set; }
+        }
+
+        public class StockStatusMetadata
+        {
+            //public int StatusID { get; set; }
+
+            [Required(ErrorMessage = "*Required field")]
+            [StringLength(25, ErrorMessage = "*Must be 25 characters or less")]
+            public string Status { get; set; }
+        }
+    }
+}
--- StoreFront.DATA.EF/StoreFrontModel.Designer.cs
+++ StoreFront.DATA.EF/StoreFrontModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Student\OneDrive\Documents\Visual Studio 2017\Projects\07_MVC2\StoreFront\StoreFront.DATA.EF\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Student\OneDrive\Documents\Visual Studio 2017\Projects\07_MVC2\StoreFront\StoreFront.DATA.EF\StoreFrontModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
